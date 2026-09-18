1af4cd4bba5ffea0f216b3a25700bd39fbcbdee6
enreysen <enreysen@go.olemiss.edu>
2026-09-18 12:02:52 -0500

    added moving wall with camera

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -26,15 +26,19 @@ func _physics_process(delta: float) -> void:
 	# idea is player keeps moving forward
 	# they move faster if looking right, slower if looking left
 	if move_input == 1.0:
+		print("looing right")
 		velocity.x = 2 * move_speed
 	elif move_input == -1.0:
 		velocity.x = move_speed * 0.5
+		print("looking left")
 	else:
 		velocity.x = move_speed 
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
+		print("jump")
 	elif Input.is_action_just_pressed("jump"):
 		velocity.y += -jump_force * .02
+		print("jump")
 		
 	move_and_slide()
