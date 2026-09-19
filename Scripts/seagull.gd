d6eaf6cd1f39b321f2eaf60e8a20ff8e94f83d7b
enreysen <enreysen@go.olemiss.edu>
2026-09-19 14:17:32 -0500

    barrel and seagull progress

---
+++ Scripts/seagull.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var move_direction: Vector2
+@export var move_speed : float = 20
+
+@onready var start_pos : Vector2 = global_position
+@onready var target_pos : Vector2 = global_position + move_direction
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	global_position = global_position.move_toward(target_pos, move_speed * delta)
+	if global_position == target_pos:
+		if target_pos == start_pos:
+			target_pos = start_pos + move_direction
+			
+		else:
+			target_pos = start_pos
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("Player"): # Replace with function body.
+		return
+		
+	print("Slow Player")
