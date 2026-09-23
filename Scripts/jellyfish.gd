7d60e22ce1f26319c1b83e4c45f68fd0e334fb92
Ember Reysen <enreysen@go.olemiss.edu>
2026-09-22 21:07:30 -0500

    randomize when animation starts for jellyfish and fish

--- Scripts/jellyfish.gd
+++ Scripts/jellyfish.gd
@@ -1,9 +1,9 @@
 extends Node
-
+@onready var anim : AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_manage_animation()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,3 +22,10 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Crab"):
 		queue_free()
+		
+func _manage_animation():
+	anim.play("jellyfish")
+	
+	# prevent all jellyfish moving in unison
+	var anim_length: float = anim.get_animation("jellyfish").length
+	anim.seek(randf_range(0.0, anim_length), true)
--- Scripts/slow_fish.gd
+++ Scripts/slow_fish.gd
@@ -6,12 +6,16 @@ extends Area2D
 @onready var start_pos : Vector2 = global_position
 @onready var target_pos : Vector2 = global_position + move_direction
 @onready var sprite : Sprite2D = $Sprite2D
+@onready var anim : AnimationPlayer = $AnimationPlayer
+@onready var node : Area2D = $"."
 
 var wall = false
+var anim_name : String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	sprite.flip_h = true
+	_manage_animation()
 
 func _process(delta: float) -> void:
 	pass
@@ -41,3 +45,17 @@ func _on_body_exited(body: Node2D) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Crab"):
 		queue_free()
+
+func _manage_animation():
+	if node.is_in_group("slow_fish_1"):
+		anim_name = "slow_fish_1"
+	elif node.is_in_group("slow_fish_2"):
+		anim_name = "slow_fish_2"
+	elif node.is_in_group("slow_fish_3"):
+		anim_name = "slow_fish_3"
+		
+	if anim_name != "":
+		# prevent all jellyfish moving in unison
+		anim.play(anim_name)
+		var anim_length: float = anim.get_animation(anim_name).length
+		anim.seek(randf_range(0.0, anim_length), true)
