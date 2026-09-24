32d20cb3e280349ba3f22cdd0c72e2e1e9ae75cf
Ember Reysen <enreysen@go.olemiss.edu>
2026-09-23 20:35:01 -0500

    update tutorial to level 1 scene

--- Scripts/tutorial_to_1.gd
+++ Scripts/tutorial_to_1.gd
@@ -44,6 +44,10 @@ func _ready() -> void:
 		big_crab.position.x += 10
 		await get_tree().create_timer(0.10).timeout
 		
+	for i in range(20):
+		big_crab.position.x += 10
+		await get_tree().create_timer(0.10).timeout
+		
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
